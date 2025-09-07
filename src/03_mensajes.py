ef808c02b587e30c5e17aad0a20953b9e15038de
equipodeprogramacion <tallerdeprogramadores@gmail.com>
2025-09-07 01:37:12 -0600

    Fix: Restore missing newline at the end of 03_mensajes.py

--- src/03_mensajes.py
+++ src/03_mensajes.py
@@ -10,4 +10,3 @@ if __name__ == '__main__':
     print(mostrar_mensaje_exito('Operación realizada con éxito.'))
     print(mostrar_mensaje_error('Formato de correo no válido.'))
     print(mostrar_mensaje_info('Recuerda guardar tu trabajo.'))
-    
