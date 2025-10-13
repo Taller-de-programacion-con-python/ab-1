a1e6e562e9c6695068245497fbe3b1401a32d5c0
JulijuanMegatilin <jmartinez17@ucol.mx>
2025-10-13 14:07:40 -0600

    Dastan 4

---
+++ actividades/Edson/fechas.py
@@ -0,0 +1,18 @@
+import datetime
+
+def dias_restantes(fecha_texto):
+    dd, mm, aaaa, = fecha_texto.split("/")
+    objetivo = fecha_texto
+    dia = int(dd)
+    mes = int(mm)
+    anio = int(aaaa)
+    hoy = datetime.now()
+    return (objetivo-hoy).days
+
+
+def estado_tareas(fecha_texto):
+    resta = dias_restantes(fecha_texto)
+    if resta < 0:
+        return "vencido"
+    if resta >=0:
+        return "por completar"
